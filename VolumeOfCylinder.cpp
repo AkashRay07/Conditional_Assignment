c9aed299c4232d9ddca1d819f91319f62dfc8c83
AkashRay07 <160709670+AkashRay07@users.noreply.github.com>
2024-08-08 09:09:24 +0530

    Add files via upload

---
+++ VolumeOfCylinder.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,h;
+    cout<<"Enter Radius :";
+    cin>>r;
+    cout<<"Enter Height :";
+    cin>>h;
+    int pi=3.1415;
+    int volumeOfCylinder=pi*r*r*h;
+    cout<<volumeOfCylinder;
+return 0;
+}
